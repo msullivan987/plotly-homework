d3f8e0e54202b6c94a670ee3cd4f20147a2a78b2
Michelle Sullivan <michelleecochran@gmail.com>
2020-12-07 22:08:43 -0800

    Created init bar chart and chart updates

--- README.md
+++ README.md
@@ -14,9 +14,7 @@ The dataset reveals that a small handful of microbial species (also called opera
 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
 * Use `sample_values` as the values for the bar chart.
-
 * Use `otu_ids` as the labels for the bar chart.
-
 * Use `otu_labels` as the hovertext for the chart.
 
   ![bar Chart](Images/hw01.png)
@@ -24,13 +22,9 @@ The dataset reveals that a small handful of microbial species (also called opera
 3. Create a bubble chart that displays each sample.
 
 * Use `otu_ids` for the x values.
-
 * Use `sample_values` for the y values.
-
 * Use `sample_values` for the marker size.
-
 * Use `otu_ids` for the marker colors.
-
 * Use `otu_labels` for the text values.
 
 ![Bubble Chart](Images/bubble_chart.png)
--- static/js/app.js
+++ static/js/app.js
@@ -1,39 +1,95 @@
+function init() {
+    d3.json("../samples.json").then(function (otuData) {
 
+        let filteredOtuData = otuData.samples.find(sample => sample.id === "940");
 
+        let topTenValues = filteredOtuData.sample_values.slice(0, 10).reverse();
 
-//helper unpack function
-function unpack(rows, index) {
-    return rows.map(function (row) {
-        return row[index];
+        let chartLabelIntegers = filteredOtuData.otu_ids.slice(0, 10).reverse();
+
+        let chartLabels = chartLabelIntegers.map(number => `OTU ${number}`);
+
+        console.log(chartLabels)
+
+        let chartHover = filteredOtuData.otu_labels.slice(0, 10).reverse();
+
+        let trace1 = {
+            y: chartLabels,
+            x: topTenValues,
+            type: "bar",
+            orientation: "h"
+        };
+
+        let data = [trace1];
+
+        Plotly.newPlot("bar", data);
     });
+
 };
 
 function updatePlotly() {
-    d3.json("../samples.json").then(function (otdData) {
-        console.log(otdData)
+    d3.json("../samples.json").then(function (otuData) {
+        console.log(otuData)
 
-        let sampleIds = otdData.names
+        //Populate the dropdown menu with the available sampleId names
+        let sampleIds = otuData.names
         let dropdownOptions = d3.select("#selDataset").selectAll("option")
 
         dropdownOptions.data(sampleIds)
             .enter()
             .append("option")
-            .text(function(d) {
+            .text(function (d) {
+                return d;
+            })
+            .property("value", function (d) {
                 return d;
             });
 
+        //How to print all the ids from the metadata object
+        //console.log(otuData.metadata.map(id => id.id));
 
-        let dataset = d3.select("#selDataset").property("value");
+        //Create event listener for changing dropdown choices
+        let dropdown = d3.select("#selDataset")
 
-        // let trace1 = {
-        //     type:"bar",
-        //     x:unpack(otdData.),
-        //     y:unpack()
-        // }
+        let datasetId = ""
 
+        function optionChanged() {
+            datasetId = d3.select("#selDataset").property("value")
+            console.log(datasetId)
+
+            //Create a filter for matching user selection to dataset
+
+            let filteredOtuData = otuData.samples.find(sample => sample.id === datasetId);
+
+            //Update Chart
+            let topTenValues = filteredOtuData.sample_values.slice(0, 10).reverse();
+
+            let chartLabelIntegers = filteredOtuData.otu_ids.slice(0, 10).reverse();
+
+            let chartLabels = chartLabelIntegers.map(number => `OTU ${number}`);
+
+            let chartHover = filteredOtuData.otu_labels.slice(0, 10).reverse();
+
+            console.log(chartLabels)
+
+            let trace1 = {
+                y: chartLabels,
+                x: topTenValues,
+                type: "bar",
+                orientation: "h"
+            };
+
+            let data = [trace1];
+
+            Plotly.react("bar", data);
+
+
+
+        };
+        dropdown.on("change", optionChanged);
     });
 
 };
 
-
+init();
 updatePlotly();
